5e3f8642818f494600b0fe635411a715f49ef1c9
Guillaume ARNAUD <60700162+guiom-arnaud@users.noreply.github.com>
2020-02-26 19:36:08 +0100

    Delete chapter.php
    Factory views :
    frontend + backend

--- view/chapter.php
+++
@@ -1,7 +0,0 @@
-<?php foreach (\App\Model\PostManager::getLast() as $post): ?>
-
-	<h2><a href="<?= $post->url; ?>"><?= $post->title; ?></a></h2>
-	<p>Publié le <?= date_format(date_create($post->created), 'd/m/Y'); ?></p>
-	<p><?= $post->excert; ?></p>
-
-<?php endforeach; ?>
